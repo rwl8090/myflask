b1f1ad945637bedc2b400b826fbde73aef7c47d9
rwl8090 <865178375@qq.com>
2019-05-26 16:29:01 +0800

    add templates files

--- myflask.py
+++ myflask.py
@@ -10,6 +10,11 @@ def hello_world():
     user_agent = request.headers.get('User-Agent')
     return '浏览器版本为：{}'.format(user_agent)
 
+@app.route('/index/')
+def index():
+
+    return render_template('index.html')
+
 @app.route('/name/<name>/')
 def get_name(name):
     return 'Hello {} ! 欢迎来到Flask！'.format(name)
@@ -29,14 +34,15 @@ def show_post(post_id):
 @app.route('/template/')
 @app.route('/template/<tmp_name>/')
 def show_tem_name(tmp_name=None):
-    return render_template('tmp_file.html', name=tmp_name)
+    ls = ['java', 'python', 'C', 'C#', 'javascript']
+    return render_template('tmp_file.html', name=tmp_name, comments=ls)
 
 
-@app.errorhandler(404)
-def not_found(error):
-    resp = Flask.make_response(render_template('error.html'), 404)
-    resp.headers['X-Parachutes'] = 'parachutes are cool'
-    return resp #render_template('error.html'), 404
+# @app.errorhandler(404)
+# def not_found(error):
+#     resp = Flask.make_response(render_template('error.html'), 404)
+#     resp.headers['X-Parachutes'] = 'parachutes are cool'
+#     return resp #render_template('error.html'), 404
 
 if __name__ == '__main__':
     app.run(debug=True)  # debug模式启动，重载器与调试器
